817ff6b66d47635bfab94fa7d155aaf018bbad33
ecampos <ecampos.jr54@gmail.com>
2023-03-26 13:08:19 -0500

    add mongoose and task model

--- models/seeds.js
+++ models/seeds.js
@@ -1,3 +1,8 @@
+//configure dotenv module in seeds.js
 const dotenv = require("dotenv");
 dotenv.config();
+
+//import mongoose and task model
+const mongoose = require('mongoose');
+const Task = require('./Task.js');
 
