def570bb629e68de770eb24baee9337c94e20a79
ecampos <ecampos.jr54@gmail.com>
2023-03-26 16:22:05 -0500

    convert callback model.create, model.findByIdAndDelete and model.findByIdAndUpdate to async/await functions

--- controllers/tasks.ctrl.js
+++ controllers/tasks.ctrl.js
@@ -1,52 +1,51 @@
 const db = require('../models')
 
 //index route showing all tasks' data in the database as json
-const index = (req, res) => {
-    db.Task.find(
-        {}, (error, allTasks) => {
-            if(error) return res.status(400).json({error: error.message});
-            
-            return res.status(200).json({
-                allTasks,
-                requestedAt: newDate().toLocaleString()
-            })
-        }
-    )
+const index = async(req, res) => {
+    try {
+        const allTasks = await db.Task.find({});
+        return res.status(200).json({
+            allTasks,
+            requestedAt: new Date().toLocaleString()
+        })
+    } catch (error) {
+        return res.status(400).json({error: error.message});
+    }
 }
 
 //create a POST route to add tasks to the task library
-const create = (req, res) => {
-    db.Task.create(
-        req.body, (error, createdTask) => {
-            if(error) return res.status(400).json({error: error.message});
-            return res.status(200).json(createdTask)
-        }
-    )
+
+const create = async(req, res) => {
+    try{
+        const createdTask = await db.Task.create(req.body);
+        return res.status(200).json(createdTask);
+    } catch (error) {
+        return res.status(400).json({error: error.message});
+    }
 }
 
 //put route
-const update = (req, res) => {
-    db.Task.findByIdAndUpdate(req.params.id,
-        {
-            $set: req.body
-        },
-        {new: true},
-        (err, updatedTask) => {
-            if(err) return res.status(400).json({error: err.message})
-            return res.status(200).json(updatedTask)
-        }
-    )
+const update = async (req, res) => {
+    try {
+        const updatedTask = await db.Task.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
+        return res.status(200).json(updatedTask);
+    } catch (err){
+        return res.status(400).json({error: err.message});
+    }
 }
 
 //delete route
-const destroy = (req, res) => {
-    db.Task.findByIdAndDelete(req.params.id, (error, deletedTask) => {
+
+const destroy = async (req, res) => {
+    try{
+        const deletedTask = await db.Task.findByIdAndDelete(req.params.id);
         if(!deletedTask) return res.status(400).json({error: "Task not found"})
-        if(error) return res.status(400).json({error: error.message})
         return res.status(200).json({
             message: `Task ${deletedTask.title} deleted`
         })
-    })
+    } catch (error) {
+        return res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = {
