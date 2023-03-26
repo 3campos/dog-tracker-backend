989532fc87911f5806970bf2837af40ee1d165e7
ecampos <ecampos.jr54@gmail.com>
2023-03-26 02:09:54 -0500

    create schema and model in task.js

---
+++ models/Task.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+//create schema
+const taskSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    label: {type: String},
+    duration: {type: String},
+    quantity: {type: String},
+    location: {type: String},
+    notes: {type: String}
+})
+
+//create model
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task
