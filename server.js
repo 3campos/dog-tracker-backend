da5232bf7c7d66c8203cbc1f35b70c90a19d6523
ecampos <ecampos.jr54@gmail.com>
2023-03-26 11:54:04 -0500

    set up variables and external modules in server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+//set up server variables/external modules
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+const cors = require ('cors');
+
