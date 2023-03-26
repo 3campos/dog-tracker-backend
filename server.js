1e06a3f38a547454ef1fe707e01f13a487840a23
ecampos <ecampos.jr54@gmail.com>
2023-03-26 12:11:20 -0500

    add middleware in server.js

--- server.js
+++ server.js
@@ -14,3 +14,26 @@ const PORT = process.env.PORT || 3003;
 //set up cors middleware
                                                  //👇deployed site link goes here
 const whitelist = [`${process.env.FRONTEND_URL}`, ``];
+const corsOptions = {
+    origin: (origin, callback) => {
+        console.log(whitelist, "WHITELIST")
+        console.log(origin, "ORIGIN")
+        if((whitelist.indexOf(origin) !== -1) || (!origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    //the below line is required for accepting credentials from the front-end, i.e., for accepting authentication.
+    //credentials: true,
+};
+
+//middleware
+//cors for cross-domain approval
+app.use(cors(corsOptions));
+app.use(express.json())
+//body data middleware
+app.use(express.urlencoded({extended: true}))
+//method override Middleware
+app.use(methodOverride("_method"))
+//serve public files
